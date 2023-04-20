e491e866ef9664069c0899c0ad80808891394979
Werneck Colins <werneck@email.com>
2023-04-20 19:04:03 -0300

    Atividade Finalizada.

---
+++ Atividade sequencial/ATV 4ª.py
@@ -0,0 +1,6 @@
+lata = int(input("Diga a quatidade em latas: ")) 
+garrafasML = int(input("Diga a quatidade em garrafasML: "))
+garrafasL = int(input("Diga a quatidade em garrafasL: "))
+soma = (lata*0.35)+(garrafasML*0.6)+(garrafasL*2)
+
+print(f"A quantidade em litros de refrigerante são {soma}L")
