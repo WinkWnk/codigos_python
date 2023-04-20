83b3cfbd00d3998500ec5a20b2c5e4ec5fb9a424
Werneck Colins <werneck@email.com>
2023-04-20 20:49:03 -0300

    Criando aula 5 sobre condicional

---
+++ aula5-condicional.py
@@ -0,0 +1,19 @@
+notaone = float (input("Informe a nota 1: "))
+notatwo = float (input("Informe a nota 2: "))
+
+average = (notaone + notatwo)/2
+
+if average >= 7: #"se" simples 
+    print("Você passou :)\n")
+
+elif media == 6: 
+    print("Você está de recuperação!")
+
+else: 
+    print("Você ficou reprovado")
+
+
+'''
+else: 
+    print ("Você reprovou :(\n")
+'''
