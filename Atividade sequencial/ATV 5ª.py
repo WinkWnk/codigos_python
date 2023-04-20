db3c3ca18e2eb4f5262ccb5899d3d6a5471dd694
Werneck Colins <werneck@email.com>
2023-04-20 19:46:46 -0300

    Atividade Finalizada

---
+++ Atividade sequencial/ATV 5ª.py
@@ -0,0 +1,5 @@
+litros = int(input("Chaves, quantos litros de refresco você quer? "))
+water = litros * 0.8
+juice = litros * 0.2
+
+print(f"O total de água é {water} e o total de suco é {juice}\n")
