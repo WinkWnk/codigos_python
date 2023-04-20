b76f516ae532e64f929dbfec37f2b8ce5cf1a2e1
Werneck Colins <werneck@email.com>
2023-04-19 21:13:45 -0300

    Atividade finalizada.

---
+++ Atividade sequencial/ATV 2ª.py
@@ -0,0 +1,4 @@
+duracao = float(input("Diga qual a duração do evento em horas: "))
+
+print(f"A duração em minutos equivale a {duracao*60}")
+print(f"A duração em segundos equivale a {duracao*36000}")
