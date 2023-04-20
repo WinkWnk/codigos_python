c44dd4a4f05bbdd922c19e797b04b7022cab671f
Werneck Colins <werneck@email.com>
2023-04-19 21:14:03 -0300

    Atividade Finalizada.

---
+++ Atividade sequencial/ATV 3ª.py
@@ -0,0 +1,5 @@
+name = input("Qual é o nome da fazenda? ")
+animals = int(input("Qual a quantidade de cavalos? "))
+ferradura = int(animals*4)
+
+print(f"A fazenda {name} possui {animals} cavalos, a quantidade total de ferradura para equipar todos os cavalos é: {ferradura} ")
