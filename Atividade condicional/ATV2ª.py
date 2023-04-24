27a97691f602d1c77fbef1ea500221be50fdf4ea
Werneck Colins <werneck@email.com>
2023-04-24 20:33:51 -0300

    Finalizado atividade condicional

---
+++ Atividade condicional/ATV2ª.py
@@ -0,0 +1,6 @@
+value = int(input("Informa o valor qualquer: "))
+
+if value >= 100 and value <= 200:
+    print(f"O valor {value} está entre 100 e 200\n")
+else:
+    print(f"O valor {value} não está entre 100 e 200\n")
