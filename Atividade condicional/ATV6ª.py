b6858312dd043b677efd3d8b6c1ff09a5e7679e7
Werneck Colins <werneck@email.com>
2023-04-24 20:36:48 -0300

    Finalizado atividade condicional

---
+++ Atividade condicional/ATV6ª.py
@@ -0,0 +1,14 @@
+percurso = float (input("Informe o percurso em km: "))
+tipoCarro = int(input("Informe o tipo do carro, 1, 2 ou 3: "))
+
+if tipoCarro == 1:
+    total = percurso / 8
+    #print(f"O tipo do carro é {tipoCarro} e vai precisar de {total} litros de gasolina\n")
+elif tipoCarro == 2:
+    total = percurso / 9
+    #print(f"O tipo do carro é {tipoCarro} e vai precisar de {total} litros de gasolina\n")
+elif tipoCarro == 3:
+    total = percurso / 12 
+    #print(f"O tipo do carro é {tipoCarro} e vai precisar de {total} litros de gasolina\n")
+
+print(f"O tipo do carro é {tipoCarro} e vai precisar de {total} litros de gasolina\n")
---
+++ Atividade condicional/ATV7ª.py
@@ -0,0 +1,16 @@
+a = int(input("Informe o 1ª valor: "))
+b = int(input("Informe o 2ª valor: "))
+c = int(input("Informe o 3ª valor: "))
+d = int(input("Informe o 4ª valor: "))
+
+if a % 2 == 0 or a % 3 == 0:
+    print(f"O valor {a} é divísivel por 2 ou 3\n")
+
+if b % 2 == 0 or a % 3 == 0:
+    print(f"O valor {b} é divísivel por 2 ou 3\n")
+
+if c % 2 == 0 or a % 3 == 0:
+    print(f"O valor {c} é divísivel por 2 ou 3\n")
+
+if d % 2 == 0 or a % 3 == 0:
+    print(f"O valor {d} é divísivel por 2 ou 3\n")
